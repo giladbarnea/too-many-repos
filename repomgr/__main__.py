0d091638366a376a39fda979c5facf80cfab0f44
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-03-11 09:25:16 +0000

    changed name to too-many-repos

--- repomgr/__main__.py
+++
@@ -1,4 +0,0 @@
-print(f'in {__file__}')
-if __name__ == "__main__":
-	from . import repomgr
-	repomgr.main()
---
+++ too_many_repos/__main__.py
@@ -0,0 +1,4 @@
+
+if __name__ == "__main__":
+	from . import too_many_repos
+	too_many_repos.main()
